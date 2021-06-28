90b73cea7a9ddf1166e8be5d5db6dd2da453facb
CepheidVariable <robertwoodard.usa@gmail.com>
2021-06-28 16:43:06 -0500

    fix: debugging prints statements, json edit

--- tacotruck.py
+++ tacotruck.py
@@ -9,15 +9,18 @@ class Entity:
 f = open(os.path.join(sys.path[0], './test.json'), "r")
 data = json.load(f)
 
-i_cust = 0
-i_truck = 0
 customers = []
 trucks = []
+
 for i in data['entities']:
     if i['customer']:
-        i_cust += 1
         customers.append(Entity(i['location'],i['customer']))
     else:
-        i_truck += 1
         trucks.append(Entity(i['location'],i['customer']))
-print(customers[0].location)
+
+# debugging
+for c in customers:
+    print(f"{customers.index(c)}: {c.location}, {c.customer} {c}")
+
+for t in trucks:
+    print(f"{trucks.index(t)}: {t.location}, {t.customer} {t}")
